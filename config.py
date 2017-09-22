2d286ea87eeab23af20d16634cb4a023b4110a3e
Daniel Bannowsky <djbannowsky@cpp.edu>
2017-09-21 23:58:27 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,11 @@
-import tweepy
-from twilio.rest import Client
-from keys import keys
-
-CONSUMER_KEY = keys['consumer_key']
-CONSUMER_SECRET = keys['consumer_secret']
-ACCESS_TOKEN = keys['access_token']
-ACCESS_TOKEN_SECRET = keys['access_token_secret']
-USER_ID = keys['user_id']
-ACCOUNT_SID = keys['account_sid']
-AUTH_TOKEN = keys['auth_token']
-
-TWILIO_PHONE_NUMBER = keys['twilio_phone_number']
-CELL_PHONE_NUMBER = keys['cell_phone_number']
-
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth)
-
-client = Client(ACCOUNT_SID, AUTH_TOKEN)
+keys = dict(
+    consumer_key='',
+    consumer_secret='',
+    access_token='',
+    access_token_secret='',
+    user_id='',
+    account_sid='',
+    auth_token='',
+    twilio_phone_number='',
+    cell_phone_number=''
+)
