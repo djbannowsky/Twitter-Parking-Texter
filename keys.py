d0b849ce65e6ba95ba090f7b0a613d3ffce5b0bb
Daniel Bannowsky <djbannowsky@cpp.edu>
2017-09-21 22:15:46 -0700

    Create keys.py

---
+++ keys.py
@@ -0,0 +1,11 @@
+keys = dict(
+    consumer_key='',
+    consumer_secret='',
+    access_token='',
+    access_token_secret='',
+    user_id='',
+    account_sid='',
+    auth_token='',
+    twilio_phone_number='',
+    cell_phone_number=''
+)
