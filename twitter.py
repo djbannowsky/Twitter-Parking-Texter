4050cb315a01fc04bf300dee4123475c2f27488e
Daniel Bannowsky <djbannowsky@cpp.edu>
2017-09-21 23:57:59 -0700

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -1,6 +1,25 @@
-from config import *
 import time
 from time import strftime
+import tweepy
+from twilio.rest import Client
+from config import keys
+
+CONSUMER_KEY = keys['consumer_key']
+CONSUMER_SECRET = keys['consumer_secret']
+ACCESS_TOKEN = keys['access_token']
+ACCESS_TOKEN_SECRET = keys['access_token_secret']
+USER_ID = keys['user_id']
+ACCOUNT_SID = keys['account_sid']
+AUTH_TOKEN = keys['auth_token']
+
+TWILIO_PHONE_NUMBER = keys['twilio_phone_number']
+CELL_PHONE_NUMBER = keys['cell_phone_number']
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
 keywords = ['parking structure 1', 'parking structure 2', 'm lot',
             'j lot', 'f-5', 'f-10', 'b-lot', 'f-3', 'f-9', 'f-10']
